7dae951937314089a419ec75ed99f3a6b71bc28b
kobe <vagrant@homestead>
2021-04-08 06:25:44 +0000

    修改和删除

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -33,4 +33,32 @@ class UserAddressesController extends Controller
         ]));
         return redirect()->route('user_addresses.index');  
     }
+
+    public function edit(UserAddress $address)
+    {
+    	$this->authorize('own',$address);
+        return view('user_addresses.create_and_edit',['address'=>$address]);
+    }
+
+    public function update(UserAddress $user_address,UserAddressRequest $request)
+    {
+    	$this->authorize('own',$user_address);
+    	$user_address->update($request->only([
+                'province',
+	            'city',
+	            'district',
+	            'address',
+	            'zip',
+	            'contact_name',
+	            'contact_phone',
+    	]));
+    	return redirect()->route('user_addresses.index');
+    }
+
+    public function destroy(UserAddress $address)
+    {
+    	$this->authorize('own',$address);
+    	$address->delete();
+    	return [];
+    }
 }
