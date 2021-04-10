b5990e628444f9eff22ca0e802ef20d167559144
kobe <vagrant@homestead>
2021-04-10 11:17:05 +0000

    用户界面商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Dcat\Admin\Traits\HasDateTimeFormatter;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -21,5 +22,14 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class,'product_id');
+    }
+
+    public function getImageUrlAttribute()
+    {
+        // 如果 image 字段本身就已经是完整的 url 就直接返回
+        if(Str::startsWith($this->attributes['image'],['http://','https://'])){
+            return $this->attributes['image'];
+        }
+        return \Storage::disk('public')->url($this->attributes['image']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::group(['middleware'=>['auth','verified']],function(){
 	Route::patch('user_addresses/{user_address}','UserAddressesController@update')->name('user_addresses.update');
 	Route::delete('user_addresses/{address}','UserAddressesController@destroy')->name('user_addresses.destroy');
 });
+Route::redirect('/','/products')->name('root');
+Route::get('products','ProductsController@index')->name('products.index');
