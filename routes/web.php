dc5f08eb886d4802e2e6c306c12f11c9c6fbdc6b
kobe <vagrant@homestead>
2021-04-12 12:43:24 +0000

    用户界面-商品详情

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,4 @@ Route::group(['middleware'=>['auth','verified']],function(){
 });
 Route::redirect('/','/products')->name('root');
 Route::get('products','ProductsController@index')->name('products.index');
+Route::get('products/{product}','ProductsController@show')->name('products.show');
