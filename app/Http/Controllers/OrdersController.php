2f6d76c3636d623691fdb721a6b670a9de12d5e5
kobe <vagrant@homestead>
2021-04-18 11:27:37 +0000

    用户界面-订单详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -71,4 +71,10 @@ class OrdersController extends Controller
     	$orders = Order::query()->with(['items.product','items.productSku'])->where('user_id',$request->user()->id)->orderBy('created_at','desc')->paginate();
     	return view('orders.index',compact('orders'));
     }
+
+    public function show(Order $order,Request $request)
+    {
+    	$this->authorize('own',$order);
+    	return view('orders.show',['order' => $order->load('items.product','items.productSku')]);
+    }
 }
