00af719017ce22fcb56302013d34994d9c2fec82
kobe <vagrant@homestead>
2021-05-04 12:39:58 +0000

    完善支付后的逻辑

---
+++ app/Events/OrderPaid.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Order;
+
+class OrderPaid
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Exceptions\InvalidRequestException;
 use Carbon\Carbon;
+use App\Events\OrderPaid;
 
 class PaymentController extends Controller
 {
@@ -61,7 +62,13 @@ class PaymentController extends Controller
         	'payment_method' => 'alipay',
         	'payment_no' => $data->trade_no,// 支付宝订单号
         ]);
+        $this->afterPaid($order);
         //\Log::debug('Alipay notify', $data->all());
         return app('alipay')->success();
+    }
+
+    public function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
     }
 }
