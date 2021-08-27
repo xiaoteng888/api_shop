5264e1a2aed6228f22c051eeb7232e0ba58c709e
kobe <vagrant@homestead>
2021-08-27 10:11:20 +0000

    用户注册

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\UserRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Exceptions\InvalidRequestException;
+use App\Http\Resources\UserResource;
+
+class UsersController extends Controller
+{
+    public function store(UserRequest $request)
+    {
+        $verifyData = \Cache::get($request->verification_key);
+
+        if(!$verifyData){
+            abort(403,'验证码已失效');
+        }
+        //hash_equals防止时序攻击的比较
+        if(!hash_equals($verifyData['code'],$request->verification_code)){
+            //返回401
+            throw new InvalidRequestException('验证码错误',401);
+        }
+        //创建用户
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => $request->password,
+        ]);
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+        return new UserResource($user);
+    }
+}
